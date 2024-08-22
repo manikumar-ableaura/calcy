1f8b6ff6edc1a8ad5210bcd665db8e94de3bffd5
manikumar <manikumar@ableaura.com>
2024-08-22 15:18:45 +0530

    Second commit of calcy

--- app/src/main/java/com/example/calcy/MainActivity2.kt
+++ app/src/main/java/com/example/calcy/MainActivity2.kt
@@ -1,12 +1,26 @@
 package com.example.calcy
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity2 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+
+        onBackPressed()
+        return true
+    }
+
+    override fun onBackPressed() {
+
+        finish()
     }
 }
